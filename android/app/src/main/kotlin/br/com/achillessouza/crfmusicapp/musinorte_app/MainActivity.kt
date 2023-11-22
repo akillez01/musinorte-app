fb5f54a8d2cad1a1d851e55ee5d68c7cf2fdf1a8
akillez01 <77950840+akillez01@users.noreply.github.com>
2023-11-21 23:38:30 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/com/achillessouza/crfmusicapp/musinorte_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.achillessouza.crfmusicapp.musinorte_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:musinorte_app/core/app/music_app.dart';
-import 'firebase_options.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 
   runApp(const MusicApp());
 }
